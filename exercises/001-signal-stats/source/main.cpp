d832c00a7288b8d177ba5fff9e2eaa6e157f1508
Paul Szczepanek <paul.szczepanek@arm.com>
2021-02-15 23:09:21 +0000

    class recorder, stats and histogram

--- exercises/001-signal-stats/source/main.cpp
+++ exercises/001-signal-stats/source/main.cpp
@@ -1,198 +1,219 @@
 #include "mbed.h"
 #include "stm32l475e_iot01_audio.h"
+#include <cmath>
 
-static uint16_t PCM_Buffer[PCM_BUFFER_LEN / 2];
-static BSP_AUDIO_Init_t MicParams;
+uint32_t isqrt32(uint32_t n)
+{
+    uint32_t root, remainder, place;
 
-static DigitalOut led(LED1);
-static EventQueue ev_queue;
+    root = 0;
+    remainder = n;
+    place = 0x40000000;
 
-// Place to store final audio (alloc on the heap), here two seconds...
-static size_t TARGET_AUDIO_BUFFER_NB_SAMPLES = AUDIO_SAMPLING_FREQUENCY * 2;
-static int16_t *TARGET_AUDIO_BUFFER = (int16_t*)calloc(TARGET_AUDIO_BUFFER_NB_SAMPLES, sizeof(int16_t));
-static size_t TARGET_AUDIO_BUFFER_IX = 0;
-
-// we skip the first 50 events (100 ms.) to not record the button click
-static size_t SKIP_FIRST_EVENTS = 50;
-static size_t half_transfer_events = 0;
-static size_t transfer_complete_events = 0;
-
-// callback that gets invoked when TARGET_AUDIO_BUFFER is full
-void target_audio_buffer_full() {
-    // pause audio stream
-    int32_t ret = BSP_AUDIO_IN_Pause(AUDIO_INSTANCE);
-    if (ret != BSP_ERROR_NONE) {
-        printf("Error Audio Pause (%d)\n", ret);
+    while (place > remainder) {
+        place = place >> 2;
     }
-    else {
-        printf("OK Audio Pause\n");
+    while (place)  {
+        if (remainder >= root + place) {
+            remainder = remainder - root - place;
+            root = root + (place << 1);
+        }
+        root = root >> 1;
+        place = place >> 2;
     }
-
-    // create WAV file
-    size_t wavFreq = AUDIO_SAMPLING_FREQUENCY;
-    size_t dataSize = (TARGET_AUDIO_BUFFER_NB_SAMPLES * 2);
-    size_t fileSize = 44 + (TARGET_AUDIO_BUFFER_NB_SAMPLES * 2);
-
-    uint8_t wav_header[44] = {
-        0x52, 0x49, 0x46, 0x46, // RIFF
-        fileSize & 0xff, (fileSize >> 8) & 0xff, (fileSize >> 16) & 0xff, (fileSize >> 24) & 0xff,
-        0x57, 0x41, 0x56, 0x45, // WAVE
-        0x66, 0x6d, 0x74, 0x20, // fmt
-        0x10, 0x00, 0x00, 0x00, // length of format data
-        0x01, 0x00, // type of format (1=PCM)
-        0x01, 0x00, // number of channels
-        wavFreq & 0xff, (wavFreq >> 8) & 0xff, (wavFreq >> 16) & 0xff, (wavFreq >> 24) & 0xff,
-        0x00, 0x7d, 0x00, 0x00, // 	(Sample Rate * BitsPerSample * Channels) / 8
-        0x02, 0x00, 0x10, 0x00,
-        0x64, 0x61, 0x74, 0x61, // data
-        dataSize & 0xff, (dataSize >> 8) & 0xff, (dataSize >> 16) & 0xff, (dataSize >> 24) & 0xff,
-    };
-
-    printf("Total complete events: %lu, index is %lu\n", transfer_complete_events, TARGET_AUDIO_BUFFER_IX);
-
-    // print both the WAV header and the audio buffer in HEX format to serial
-    // you can use the script in `hex-to-buffer.js` to make a proper WAV file again
-    printf("WAV file:\n");
-    for (size_t ix = 0; ix < 44; ix++) {
-        printf("%02x", wav_header[ix]);
-    }
-
-    uint8_t *buf = (uint8_t*)TARGET_AUDIO_BUFFER;
-    for (size_t ix = 0; ix < TARGET_AUDIO_BUFFER_NB_SAMPLES * 2; ix++) {
-        printf("%02x", buf[ix]);
-    }
-    printf("\n");
+    return root;
 }
 
-/**
-* @brief  Half Transfer user callback, called by BSP functions.
-* @param  None
-* @retval None
-*/
-void BSP_AUDIO_IN_HalfTransfer_CallBack(uint32_t Instance) {
-    half_transfer_events++;
-    if (half_transfer_events < SKIP_FIRST_EVENTS) return;
+class Recorder
+{
+public:
+    /* buffer sizes */
+    static constexpr size_t DRIVER_BUFFER_SIZE_IN_MS = 100;
+    static constexpr size_t APP_BUFFER_SIZE_IN_MS = 2000;
+    static constexpr size_t CHUNK_SIZE_IN_MS = 500;
 
-    uint32_t buffer_size = PCM_BUFFER_LEN / 2; /* Half Transfer */
-    uint32_t nb_samples = buffer_size / sizeof(int16_t); /* Bytes to Length */
+    static constexpr size_t BYTES_PER_SAMPLE = 2;
 
-    if ((TARGET_AUDIO_BUFFER_IX + nb_samples) > TARGET_AUDIO_BUFFER_NB_SAMPLES) {
-        return;
-    }
+    static constexpr size_t DRIVER_SAMPLES = (AUDIO_SAMPLING_FREQUENCY * DRIVER_BUFFER_SIZE_IN_MS) / 1000;
+    static constexpr size_t DRIVER_BYTES = DRIVER_SAMPLES * BYTES_PER_SAMPLE;
+    static constexpr size_t APP_SAMPLES = (AUDIO_SAMPLING_FREQUENCY * APP_BUFFER_SIZE_IN_MS) / 1000;
+    static constexpr size_t APP_BYTES = APP_SAMPLES * BYTES_PER_SAMPLE;
+    static constexpr size_t CHUNK_SAMPLES = (AUDIO_SAMPLING_FREQUENCY * CHUNK_SIZE_IN_MS) / 1000;
+    static constexpr size_t CHUNK_BYTES = CHUNK_SAMPLES * BYTES_PER_SAMPLE;
 
-    /* Copy first half of PCM_Buffer from Microphones onto Fill_Buffer */
-    memcpy(((uint8_t*)TARGET_AUDIO_BUFFER) + (TARGET_AUDIO_BUFFER_IX * 2), PCM_Buffer, buffer_size);
-    TARGET_AUDIO_BUFFER_IX += nb_samples;
+    Recorder(EventQueue& event_queue) : queue(event_queue) {};
+    int init() {
+        driver_buffer = (uint8_t *) malloc(DRIVER_BYTES);
+        app_buffer = (uint16_t *) malloc(APP_BYTES);
+        
+        if (!driver_buffer || !app_buffer) {
+            printf("Failed to allocate app_buffer buffer\r\n");
+            return 1;
+        }
+        
+        BSP_AUDIO_Init_t MicParams;
+        MicParams.BitsPerSample = BYTES_PER_SAMPLE * 8;
+        MicParams.ChannelsNbr = AUDIO_CHANNELS;
+        MicParams.Device = AUDIO_IN_DIGITAL_MIC1;
+        MicParams.SampleRate = AUDIO_SAMPLING_FREQUENCY; //16Khz
+        MicParams.Volume = 32;
 
-    if (TARGET_AUDIO_BUFFER_IX >= TARGET_AUDIO_BUFFER_NB_SAMPLES) {
-        ev_queue.call(&target_audio_buffer_full);
-        return;
-    }
-}
+        int32_t ret = BSP_AUDIO_IN_Init(AUDIO_INSTANCE, &MicParams);
 
-/**
-* @brief  Transfer Complete user callback, called by BSP functions.
-* @param  None
-* @retval None
-*/
-void BSP_AUDIO_IN_TransferComplete_CallBack(uint32_t Instance) {
-    transfer_complete_events++;
-    if (transfer_complete_events < SKIP_FIRST_EVENTS) return;
+        if (ret != BSP_ERROR_NONE) {
+            printf("Error Audio Init (%ld)\r\r\n", ret);
+            return 1;
+        } else {
+            printf("OK Audio Init\t(Audio Freq=%ld)\r\r\n", AUDIO_SAMPLING_FREQUENCY);
+        }
 
-    uint32_t buffer_size = PCM_BUFFER_LEN / 2; /* Half Transfer */
-    uint32_t nb_samples = buffer_size / sizeof(int16_t); /* Bytes to Length */
-
-    if ((TARGET_AUDIO_BUFFER_IX + nb_samples) > TARGET_AUDIO_BUFFER_NB_SAMPLES) {
-        return;
-    }
-
-    /* Copy second half of PCM_Buffer from Microphones onto Fill_Buffer */
-    memcpy(((uint8_t*)TARGET_AUDIO_BUFFER) + (TARGET_AUDIO_BUFFER_IX * 2),
-        ((uint8_t*)PCM_Buffer) + (nb_samples * 2), buffer_size);
-    TARGET_AUDIO_BUFFER_IX += nb_samples;
-
-    if (TARGET_AUDIO_BUFFER_IX >= TARGET_AUDIO_BUFFER_NB_SAMPLES) {
-        ev_queue.call(&target_audio_buffer_full);
-        return;
-    }
-}
-
-/**
-  * @brief  Manages the BSP audio in error event.
-  * @param  Instance Audio in instance.
-  * @retval None.
-  */
-void BSP_AUDIO_IN_Error_CallBack(uint32_t Instance) {
-    printf("BSP_AUDIO_IN_Error_CallBack\n");
-}
-
-void print_stats() {
-    printf("Half %lu, Complete %lu, IX %lu\n", half_transfer_events, transfer_complete_events,
-        TARGET_AUDIO_BUFFER_IX);
-}
-
-void start_recording() {
-    int32_t ret;
-    uint32_t state;
-
-    ret = BSP_AUDIO_IN_GetState(AUDIO_INSTANCE, &state);
-    if (ret != BSP_ERROR_NONE) {
-        printf("Cannot start recording: Error getting audio state (%d)\n", ret);
-        return;
-    }
-    if (state == AUDIO_IN_STATE_RECORDING) {
-        printf("Cannot start recording: Already recording\n");
-        return;
-    }
-
-    // reset audio buffer location
-    TARGET_AUDIO_BUFFER_IX = 0;
-    transfer_complete_events = 0;
-    half_transfer_events = 0;
-
-    ret = BSP_AUDIO_IN_Record(AUDIO_INSTANCE, (uint8_t *) PCM_Buffer, PCM_BUFFER_LEN);
-    if (ret != BSP_ERROR_NONE) {
-        printf("Error Audio Record (%ld)\n", ret);
-        return;
-    }
-    else {
-        printf("OK Audio Record\n");
-    }
-}
-
-void start_recording_btn_cb() {
-    ev_queue.call([](){start_recording();});
-}
-
-int main() {
-    printf("Hello from the B-L475E-IOT01A microphone demo\n");
-
-    if (!TARGET_AUDIO_BUFFER) {
-        printf("Failed to allocate TARGET_AUDIO_BUFFER buffer\n");
         return 0;
     }
 
-    // set up the microphone
-    MicParams.BitsPerSample = 16;
-    MicParams.ChannelsNbr = AUDIO_CHANNELS;
-    MicParams.Device = AUDIO_IN_DIGITAL_MIC1;
-    MicParams.SampleRate = AUDIO_SAMPLING_FREQUENCY;
-    MicParams.Volume = 32;
+    void record() {
+        int32_t ret;
+        uint32_t state;
 
-    int32_t ret = BSP_AUDIO_IN_Init(AUDIO_INSTANCE, &MicParams);
+        ret = BSP_AUDIO_IN_GetState(AUDIO_INSTANCE, &state);
+        if (ret != BSP_ERROR_NONE) {
+            printf("Cannot start recording: Error getting audio state (%d)\r\n", ret);
+            return;
+        }
+        if (state == AUDIO_IN_STATE_RECORDING) {
+            printf("Cannot start recording: Already recording\r\n");
+            return;
+        }
 
-    if (ret != BSP_ERROR_NONE) {
-        printf("Error Audio Init (%ld)\r\n", ret);
-        return 1;
-    } else {
-        printf("OK Audio Init\t(Audio Freq=%ld)\r\n", AUDIO_SAMPLING_FREQUENCY);
+        app_buffer_index = 0;
+
+        ret = BSP_AUDIO_IN_Record(AUDIO_INSTANCE, driver_buffer, DRIVER_BYTES);
+        if (ret != BSP_ERROR_NONE) {
+            printf("Error Audio Record (%ld)\r\n", ret);
+            return;
+        } else {
+            printf("OK Audio Record\r\n");
+        }
     }
 
-    printf("Press the BLUE button to record a message\n");
+    void stop() {
+        int32_t ret = BSP_AUDIO_IN_Stop(AUDIO_INSTANCE);
+        if (ret != BSP_ERROR_NONE) {
+            printf("Error Audio Stop (%d)\r\n", ret);
+        }
+    }
 
-    // hit the blue button to record a message
-    static InterruptIn btn(BUTTON1);
-    btn.fall(&start_recording_btn_cb);
+    /* every time we copy half the driver buffer, when full is true we copy the second half */
+    void audio_in(bool full) {
+        if (skip_events) {
+            skip_events--;
+            return;
+        }
 
-    ev_queue.dispatch_forever();
+        const uint8_t *source = full ? driver_buffer + DRIVER_BYTES / 2 : driver_buffer;
+        memcpy(app_buffer + app_buffer_index, source, DRIVER_BYTES / 2);
+
+        accumulated_chunk += DRIVER_SAMPLES / 2;
+        app_buffer_index += DRIVER_SAMPLES / 2;
+
+        /* TODO: measure how much analysis is lagging behind capture to skip frames */
+
+        app_buffer_index %= APP_SAMPLES;
+
+        if (accumulated_chunk >= CHUNK_SAMPLES) {
+            accumulated_chunk = 0;
+            chunk_index = (app_buffer_index - CHUNK_SAMPLES);
+            chunk_index %= APP_SAMPLES;
+            queue.call(this, &Recorder::analyse_buffer);
+            return;
+        }
+    }
+
+private:
+    void analyse_buffer() {
+        const uint16_t *source = app_buffer + chunk_index;
+
+        /* average */
+        uint32_t sum = 0;
+        uint16_t sum_avg = source[0];
+
+        /* standard deviation */
+        int32_t M2 = 0;
+        int32_t delta = 0;
+
+        /* histogram buckets */
+        const size_t n_bucket = 8;
+        const size_t bucket_size = 2000;
+        size_t samples_in_bucket[n_bucket] = { 0 };
+        
+        for (size_t n = 0; n < CHUNK_SAMPLES; n++) {
+            const uint16_t value = source[n];
+
+            /* average */
+            sum += value;
+            uint16_t sum_avg = sum / (n + 1);
+
+            /* standard deviation */
+            const int32_t new_delta = (int32_t)value - (int32_t)sum_avg;
+            M2 += delta * new_delta;
+            delta = new_delta;
+
+            /* histogram */
+            size_t bucket_idx = (value / bucket_size);
+            if (bucket_idx >= n_bucket) {
+                bucket_idx = n_bucket - 1;
+            }
+            samples_in_bucket[bucket_idx]++;
+        }
+
+        const uint32_t variance = std::abs((int32_t)(M2 / CHUNK_SAMPLES));
+        const uint32_t dev = isqrt32(variance);
+
+        printf("sum %016lu sum_avg %08lu dev %08d\r\n", sum, sum_avg, dev);
+
+        // histogram
+        printf("%04lu %04lu %04lu %04lu %04lu %04lu %04lu %04lu\r\n",
+               samples_in_bucket[0], samples_in_bucket[1], samples_in_bucket[2], samples_in_bucket[3],
+               samples_in_bucket[4], samples_in_bucket[5], samples_in_bucket[6], samples_in_bucket[7]
+        );
+    }
+
+private:
+    EventQueue& queue;
+    uint8_t *driver_buffer = nullptr;
+
+    /* TODO: replace with circular buffer */
+    uint16_t *app_buffer = nullptr;
+    size_t app_buffer_index = 0;
+    size_t chunk_index = 0;
+    size_t accumulated_chunk = 0;
+
+    /* first recorder full driver buffer will be skipped */
+    size_t skip_events = 2;
+};
+
+static EventQueue event_queue;
+static Recorder recorder(event_queue);
+
+/* these are hardcoded in the driver to handle interrupts */
+void BSP_AUDIO_IN_HalfTransfer_CallBack(uint32_t Instance) {
+    recorder.audio_in(false);
+}
+
+void BSP_AUDIO_IN_TransferComplete_CallBack(uint32_t Instance) {
+    recorder.audio_in(true);
+}
+
+void BSP_AUDIO_IN_Error_CallBack(uint32_t Instance) {
+    printf("BSP_AUDIO_IN_Error_CallBack\r\n");
+}
+
+int main() {
+    int ret = recorder.init();
+    if (ret) {
+        return ret;
+    }
+
+    recorder.record();
+
+    event_queue.dispatch_forever();
 }
